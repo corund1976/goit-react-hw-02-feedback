6437e45a51a18999f51f60a1a0c944c6c209569a
Sergii Budak <corund1976@gmail.com>
2021-11-01 19:43:29 +0200

    final

---
+++ src/components/FeedbackOptions/Buttons/Buttons.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import s from './Buttons.module.css';
+
+function Buttons({ options, onLeaveFeedback }) {
+  return (
+    options.map(item => 
+      <li className={s.buttonsListItem} key={item}>
+        <button
+          type="button"
+          className={s.button}
+          name={item}
+          onClick={onLeaveFeedback}>
+          {item.charAt(0).toUpperCase() + item.slice(1)}
+        </button>
+      </li>
+    )
+  );
+};
+
+Buttons.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+}
+
+export default Buttons;
